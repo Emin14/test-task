a856304639ea4951d43e63f8ad831520d8ad0ccd
Emin14 <emain-agjaev@mail.ru>
2024-03-12 10:35:43 +0300

    added a clickable logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,22 @@
 import logo from "../../assets/logo.svg";
+import { useDispatch } from "react-redux";
+import { changeCurrentPage } from "../../redux/slices/currentPageSlice";
+import { clearParams } from "../../redux/slices/paramsSlice";
 import styles from "./header.module.css";
 
 export default function Header() {
+
+  const dispatch = useDispatch();
+
+  function handleClick() {
+    dispatch(clearParams());
+    dispatch(changeCurrentPage(1));
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <img src={logo} alt="" />
+        <img  className={styles.logo} src={logo} alt="" onClick={handleClick}/>
         <ul className={styles.list}>
           <li>Каталог</li>
           <li>Займ</li>
